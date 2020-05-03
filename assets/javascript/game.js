5ba25f4a8b29d93bf863ee116774655369c83a57
Yoon Ho Ryoo <ryoo94@gmail.com>
2020-05-02 23:56:27 -0700

    Cleaned up code a lil bit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,41 +31,37 @@ $('#loss').html(loss);
 // Checks whether or not both scores are equal or greater & then displays win or loss 
 $(".img-thumbnail").click(function(){
     if(myNum === compNum){
-        win++;
         alert("You won!")
+        win++;
         $('#win').html(win);
         
         compNum = (Math.floor(Math.random() * (101) + 19));
         $('#targetNum').html(compNum);
 
         red = (Math.floor(Math.random() * (12) + 1));
-        console.log("Red: " + red);
         green = (Math.floor(Math.random() * (12) + 1));
-        console.log("Green: " + green);
         blue = (Math.floor(Math.random() * (12) + 1));
-        console.log("Blue: " + blue);
         yellow = (Math.floor(Math.random() * (12) + 1));
-        console.log("Yellow: " + yellow);
+
+        logNums();
 
         myNum = 0;
         $('#myNum').html(myNum);
     }
     if(myNum > compNum){
-        loss++;
         alert("You lost! Try again?")
+        loss++;
         $('#loss').html(loss);
         
         compNum = (Math.floor(Math.random() * (101) + 19));
         $('#targetNum').html(compNum);
 
         red = (Math.floor(Math.random() * (12) + 1));
-        console.log("Red: " + red);
         green = (Math.floor(Math.random() * (12) + 1));
-        console.log("Green: " + green);
         blue = (Math.floor(Math.random() * (12) + 1));
-        console.log("Blue: " + blue);
         yellow = (Math.floor(Math.random() * (12) + 1));
-        console.log("Yellow: " + yellow);
+
+        logNums();
 
         myNum = 0;
         $('#myNum').html(myNum);
